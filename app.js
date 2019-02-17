df1cc9f8fe9df0205353a0fb9e89cdf379dcc76d
omerme13 <40212089+omerme13@users.noreply.github.com>
2019-02-17 21:11:39 +0200

    17.2.19

--- app.js
+++ app.js
@@ -2,181 +2,165 @@ const taskList = document.querySelector('.curr-tasks');
 const finishedTaskList = document.querySelector('.finished-tasks');
 const addButton = document.querySelector('.add-task');
 const clearButton = document.querySelector('.clear-tasks');
-const filteInput = document.querySelector('#filter-input');
+const filterInput = document.querySelector('#filter-input');
 const taskInput = document.querySelector('#user-input');
 
 /* ----------------------------- */
-/* ----- HELPING FUNCTIONS  ----- */
-function AddIconToTask(listItem)
-{
+/* ----- HELPING FUNCTIONS ----- */
+function addIconToTask(listItem) {
     const icon = document.createElement('a');
     icon.className = 'ion-trash-a delete-task icon-delete icons';
     listItem.appendChild(icon);
 }
 
-function AddTask(listName, content)
-{
+function addTask(listName, content) {
     const li = document.createElement('li');
 
     li.appendChild(document.createTextNode(content));
-    AddIconToTask(li);
+    addIconToTask(li);
     listName.appendChild(li);
 }
 
-function InitTasksInLS(tasks)
-{
-    if (localStorage.getItem('tasks') === null)
+function initTasksInLS(tasks) {
+    if (localStorage.getItem('tasks') === null) {
         return tasks = [];
-    else
+    } else {
         return tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
 }
 
-function InitFinishedTasksInLS(finishedTasks)
-{
-    if (localStorage.getItem('finishedTasks') === null)
+function initFinishedTasksInLS(finishedTasks) {
+    if (localStorage.getItem('finishedTasks') === null) {
         return finishedTasks = [];
-    else
+    } else {
         return finishedTasks = JSON.parse(localStorage.getItem('finishedTasks'));
+    }
 }
 
-function RemoveTaskFromLS(task)
-{
+function removeTaskFromLS(task) {
     let tasks;
 
-    tasks = InitTasksInLS(tasks);
-    tasks.forEach(function(taskItem, index)
-    {
-        if (task.textContent === taskItem)
+    tasks = initTasksInLS(tasks);
+    tasks.forEach(function(taskItem, index) {
+        if (task.textContent === taskItem) {
             tasks.splice(index, 1);
+        }
     });
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-function RemoveFinishedTaskFromLS(task)
-{
+function removeFinishedTaskFromLS(task) {
     let finishedTasks;
 
-    finishedTasks = InitFinishedTasksInLS(finishedTasks);
-    finishedTasks.forEach(function(taskItem, index)
-    {
-        if (task.textContent === taskItem)
+    finishedTasks = initFinishedTasksInLS(finishedTasks);
+    finishedTasks.forEach(function(taskItem, index) {
+        if (task.textContent === taskItem) {
             finishedTasks.splice(index, 1);
+        }
     });
     localStorage.setItem('finishedTasks', JSON.stringify(finishedTasks));
 }
 
-function StoreInLS(task)
-{
+function storeInLS(task) {
     let tasks;
 
-    tasks = InitTasksInLS(tasks);
+    tasks = initTasksInLS(tasks);
     tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-function AddTaskToTaskList()
-{
-    AddTask(taskList, taskInput.value);
-    StoreInLS(taskInput.value);
+function addTaskToTaskList() {
+    addTask(taskList, taskInput.value);
+    storeInLS(taskInput.value);
     taskInput.value = '';
 }
 
-function AddTaskToFinished(task)
-{
-    AddTask(finishedTaskList, task.textContent);
+function addTaskToFinished(task) {
+    addTask(finishedTaskList, task.textContent);
 }
 
-/* ----- HELPING FUNCTIONS  ----- */
 /* ----------------------------- */
 
-function AddAfterClick()
-{
-    if (taskInput.value.length > 0)
-        AddTaskToTaskList();
+function addAfterClick() {
+    if (taskInput.value.length > 0) {
+        addTaskToTaskList();
+    }
 }
 
-function AddAfterKeyPress()
-{
-    if (taskInput.value.length > 0 && event.which == 13)
-        AddTaskToTaskList();
+function addAfterKeyPress() {
+    if (taskInput.value.length > 0 && event.which == 13) {
+        addTaskToTaskList();
+    }
 }
 
-function DeleteListItem(e)
-{
+function deleteListItem(e) {
     const task = e.target;
 
-    if (task.parentElement.classList.contains('curr-tasks'))
-    {
+    if (task.parentElement.classList.contains('curr-tasks')) {
         let finishedTasks;
 
-        AddTaskToFinished(task);
-        RemoveTaskFromLS(task);
-        finishedTasks = InitFinishedTasksInLS(finishedTasks);
+        addTaskToFinished(task);
+        removeTaskFromLS(task);
+        finishedTasks = initFinishedTasksInLS(finishedTasks);
         finishedTasks.push(task.textContent);
         localStorage.setItem('finishedTasks', JSON.stringify(finishedTasks));
         task.remove();
     }
 
-    if (task.classList.contains('icon-delete'))
-    {
-        if (task.parentElement.parentElement.classList.contains('curr-tasks'))
-            RemoveTaskFromLS(task.parentElement);
-        else
-            RemoveFinishedTaskFromLS(task.parentElement);
+    if (task.classList.contains('icon-delete')) {
+        if (task.parentElement.parentElement.classList.contains('curr-tasks')) {
+            removeTaskFromLS(task.parentElement);
+        }
+        else {
+            removeFinishedTaskFromLS(task.parentElement);
+        }
         task.parentElement.remove();
     }
 }
 
-function ClearFinished()
-{
-    while (finishedTaskList.firstChild)
-    {
-        RemoveFinishedTaskFromLS(finishedTaskList.firstChild);
+function clearFinished() {
+    while (finishedTaskList.firstChild) {
+        removeFinishedTaskFromLS(finishedTaskList.firstChild);
         finishedTaskList.removeChild(finishedTaskList.firstChild);
     }
 }
 
-function FilterTasks(e)
-{
+function filterTasks(e) {
     const text = e.target.value.toLowerCase();
 
-    document.querySelectorAll('li').forEach(function(task)
-    {
+    document.querySelectorAll('li').forEach(function(task) {
         const item = task.firstChild.textContent;
 
-        if (item.toLowerCase().indexOf(text) > -1)
+        if (item.toLowerCase().indexOf(text) > -1) {
             task.style.display = 'flex';
-        else
+        } else {
             task.style.display = 'none';
+        }
     });
 }
 
-function GetTasks()
-{
+function getTasks() {
     let tasks, finishedTasks;
 
-    tasks = InitTasksInLS(tasks);
-    finishedTasks = InitFinishedTasksInLS(finishedTasks);
+    tasks = initTasksInLS(tasks);
+    finishedTasks = initFinishedTasksInLS(finishedTasks);
 
-    tasks.forEach(function(task)
-    {
-        AddTask(taskList, task);
+    tasks.forEach(function(task) {
+        addTask(taskList, task);
     });
-    finishedTasks.forEach(function(task)
-    {
-        AddTask(finishedTaskList, task);
+    finishedTasks.forEach(function(task) {
+        addTask(finishedTaskList, task);
     });
 }
 
-function loadEventListeners()
-{
-    addButton.addEventListener('click', AddAfterClick);
-    taskInput.addEventListener('keypress', AddAfterKeyPress);
-    taskList.addEventListener('click', DeleteListItem);
-    finishedTaskList.addEventListener('click', DeleteListItem);
-    clearButton.addEventListener('click', ClearFinished);
-    filteInput.addEventListener('keyup', FilterTasks);
-    document.addEventListener('DOMContentLoaded', GetTasks);
+function loadEventListeners() {
+    addButton.addEventListener('click', addAfterClick);
+    taskInput.addEventListener('keypress', addAfterKeyPress);
+    taskList.addEventListener('click', deleteListItem);
+    finishedTaskList.addEventListener('click', deleteListItem);
+    clearButton.addEventListener('click', clearFinished);
+    filterInput.addEventListener('keyup', filterTasks);
+    document.addEventListener('DOMContentLoaded', getTasks);
 }
 
 loadEventListeners();
